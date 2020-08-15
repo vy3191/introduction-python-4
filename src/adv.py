175eb2bf353cf6aecc05fddeedd871de3ad9e193
vy3191 <venkyafhs@gmail.com>
2020-08-15 15:24:38 -0400

    Modified items file

--- src/adv.py
+++ src/adv.py
@@ -1,6 +1,6 @@
 from room import Room
 from player import Player
-# from item import Item
+from item import Item
 
 
 # Declare all the rooms
@@ -36,7 +36,11 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
+room['outside'].items.append(Item('knife', 'A sharp item to cut things'))
+room['outside'].items.append(Item('light', 'which give a light in the room'))
 
+room['foyer'].items.append(Item('pencil', 'Something to write with'))
+room['foyer'].items.append(Item('paper', 'where you write'))
 
 #
 # Main
@@ -52,23 +56,26 @@ while True:
     print(f" You are current location is at {player.current_room.name} room.")
 # * Prints the current description (the textwrap module might be useful here).
     print(f"and it looks like: {player.current_room.description}")
+
+    if len(player.current_room.items) > 0:
+        # print items in the room.
+        print("The room container the following items:")
+        for item in player.current_room.items:
+            print(item)
+    else:
+        print("This room has no items for you to carry")        
 # * Waits for user input and decides what to do.
     user_input = input("If you want to move to a different room please choose a direction ('N', 'S','E','W'):\n")
-# If the user enters a cardinal direction, attempt to move to the room there.
-    if user_input.lower() == 'q':
-        break
+# If the user enters a cardinal direction, attempt to move to the room there.    
     if user_input.lower():
         attribute = user_input.lower()+"_to"
         if hasattr(player.current_room, attribute):
-            player.current_room = getattr(player.current_room, attribute)
+            player.current_room = getattr(player.current_room, attribute)        
+        # Print an error message if the movement isn't allowed.    
         else:
             print(f"The {player.current_room.name} does not have {user_input} direction.")
             print(f"Please choose a different direction.")
-            continue
-
-            
-
-
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
+            continue 
+    # If the user enters "q", quit the game.
+    if user_input.lower() == 'q':
+        break
